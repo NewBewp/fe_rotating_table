b9eeb9d3d105ef7fc785f811160de985ab57f5aa
HaKien <nguyenhakien99@gmail.com>
2024-01-16 21:42:53 +0700

    hien so goc

--- src/components/templates/angle/AngleInput.tsx
+++ src/components/templates/angle/AngleInput.tsx
@@ -1,5 +1,33 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const AngleInput = () => {
+  const [myData, setMyData] = useState([]);
+  const getAngle = async () => {
+    try {
+      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/getlatestdata');
+      setMyData(response.data);
+      console.log("data", response.data[0].number);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  useEffect(() => {
+    getAngle();
+  }, [setMyData]);
+
   return (
-    <div>AngleInput</div>
+    <div>
+      <div>
+        Góc: {myData.map(item => item.number)}
+      </div>
+      <div>
+        Tiếng của: ?
+      </div>
+      <div>
+        Thời gian: ?
+      </div>
+    </div>
   )
 }
--- src/pages/Angle.tsx
+++ src/pages/Angle.tsx
@@ -3,7 +3,7 @@ import { AngleTemplate } from "components"
 export const Angle = () => {
   return (
     <div>
-      <div className=" text-center ">
+      <div className="text-center">
         Góc Độ
       </div>
       <AngleTemplate />
