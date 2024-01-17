0ca9fca4494eab509bfe5a48a2ed90a588c923ee
HaKien <nguyenhakien99@gmail.com>
2024-01-17 17:54:41 +0700

    sound type, time

--- src/components/templates/angle/AngleInput.tsx
+++ src/components/templates/angle/AngleInput.tsx
@@ -2,31 +2,64 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export const AngleInput = () => {
-  const [myData, setMyData] = useState([]);
+  const [myAngle, setMyAngle] = useState([]);
+  const [mySound, setMySound] = useState([]);
+  const [myTime, setMyTime] = useState([]);
+
   const getAngle = async () => {
     try {
       const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/getlatestdata');
-      setMyData(response.data);
+      setMyAngle(response.data);
       console.log("data", response.data[0].number);
     } catch (error) {
       console.error('Error fetching data:', error);
     }
   };
 
+  const getSound = async () => {
+    try {
+      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/getsoundtype');
+      setMySound(response.data);
+      console.log("data", response.data[0].Public.Input.Data.Prediction);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const getTime = async () => {
+    try {
+      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/application-0-rxqgf/endpoint/get_time');
+      setMyTime(response.data);
+      console.log("data", response.data[0].Public.Input.Data.Time);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  
+  useEffect(() => {
+    getSound();
+  }, [mySound]);
+
   useEffect(() => {
     getAngle();
-  }, [setMyData]);
+  }, [myAngle]);
+
+  useEffect(() => {
+    getTime();
+  }, [myTime]);
+
+
 
   return (
     <div>
       <div>
-        Góc: {myData.map(item => item.number)}
+        Thời gian: {myTime.map(item => item.Public.Input.Data.Time)}
       </div>
       <div>
-        Tiếng của: ?
+        Góc: {myAngle.map(item => item.number)} độ
       </div>
       <div>
-        Thời gian: ?
+        Tiếng của: {mySound.map(item => item.Public.Input.Data.Prediction)}
       </div>
     </div>
   )
