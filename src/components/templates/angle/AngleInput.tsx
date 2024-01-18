040d0c94a883c70cbb4dbf578a1af4f628cfa083
HaKien <nguyenhakien99@gmail.com>
2024-01-18 18:03:28 +0700

    add table

--- src/components/templates/angle/AngleInput.tsx
+++ src/components/templates/angle/AngleInput.tsx
@@ -35,7 +35,7 @@ export const AngleInput = () => {
       console.error('Error fetching data:', error);
     }
   };
-  
+
   useEffect(() => {
     getSound();
   }, [mySound]);
@@ -61,6 +61,24 @@ export const AngleInput = () => {
       <div>
         Tiếng của: {mySound.map(item => item.Public.Input.Data.Prediction)}
       </div>
+
+
+      <table className="table-fixed border-solid border-2 border-black">
+        <thead>
+          <tr className="border-b-2 border-black ">
+            <th className="px-2 border-r-2 border-black">Thời gian</th>
+            <th className="px-2 border-r-2 border-black">Góc</th>
+            <th className="px-2 border-r-2 border-black">Âm Thanh</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="px-2 border-r-2 border-black">{myTime.map(item => item.Public.Input.Data.Time)}</td>
+            <td className="px-2 border-r-2 border-black">{myAngle.map(item => item.number)}</td>
+            <td className="px-2 border-r-2 border-black">{mySound.map(item => item.Public.Input.Data.Prediction)}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
